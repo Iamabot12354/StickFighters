f05467a093cdd3f78ace28e5b494ec369d95f8c1
Iamabot12354 <shawn@marsman.ca>
2025-12-10 18:31:50 -0500

    Making Progress

--- stickfighters/stickfighhter/Scripts/character_body_2d.gd
+++ stickfighters/stickfighhter/Scripts/character_body_2d.gd
@@ -13,7 +13,7 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("P1-Up") and is_on_floor():
+	if Input.is_action_just_pressed("P1-Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
