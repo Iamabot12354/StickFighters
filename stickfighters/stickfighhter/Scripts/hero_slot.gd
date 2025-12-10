176e72b0b55c3702bd0354021f5c5dc9efc510d7
Iamabot12354 <shawn@marsman.ca>
2025-12-10 18:49:48 -0500

    Added easy charecter menu selection

--- stickfighters/stickfighhter/Scripts/hero_slot.gd
+++ stickfighters/stickfighhter/Scripts/hero_slot.gd
@@ -1,10 +1,18 @@
 extends Node2D
 
 
+enum charecters {
+	Dummy,
+	New,
+}
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
+	
+@export var This_Charecter : charecters = charecters.Dummy
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
