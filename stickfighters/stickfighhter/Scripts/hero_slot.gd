70c43f5c0c67b97b632ae295339cd822d1822af1
Iamabot12354 <shawn@marsman.ca>
2025-12-10 22:15:06 -0500

    Working on controllers

--- stickfighters/stickfighhter/Scripts/hero_slot.gd
+++ stickfighters/stickfighhter/Scripts/hero_slot.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
+var joined = []
+
 
 enum charecters {
 	Dummy,
-	New,
+	Sword,
 }
 
-
+var device_1 = -1
+var device_2 = -1
+var device_3 = -1
+var device_4 = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
